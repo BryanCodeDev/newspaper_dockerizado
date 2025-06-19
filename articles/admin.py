d4b3bc13b13ad5f8da5b06ba02f60eda34550cf9
Bryan Muñoz <tiagotroller021019@gmail.com>
2025-06-18 20:55:11 -0500

    nespaper dockerizado y terminado

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,45 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Article
 
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date', 'has_image_display')
+    list_filter = ('date', 'author')
+    search_fields = ('title', 'body')
+    readonly_fields = ('date', 'image_preview')
+    
+    fieldsets = (
+        ('Información Principal', {
+            'fields': ('title', 'author', 'body')
+        }),
+        ('Imagen', {
+            'fields': ('image', 'image_preview'),
+            'classes': ('collapse',)
+        }),
+        ('Metadatos', {
+            'fields': ('date',),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def has_image_display(self, obj):
+        """Muestra si el artículo tiene imagen"""
+        if obj.has_image:
+            return format_html(
+                '<span style="color: green;">✓ Sí</span>'
+            )
+        return format_html(
+            '<span style="color: red;">✗ No</span>'
+        )
+    has_image_display.short_description = 'Tiene Imagen'
+    
+    def image_preview(self, obj):
+        """Muestra una vista previa de la imagen en el admin"""
+        if obj.has_image:
+            return format_html(
+                '<img src="{}" style="max-width: 300px; max-height: 200px; border-radius: 8px;" />',
+                obj.image.url
+            )
+        return "No hay imagen"
+    image_preview.short_description = 'Vista Previa'
--- articles/models.py
+++ articles/models.py
@@ -2,10 +2,27 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+import os
+
+def article_image_path(instance, filename):
+    """Función para generar la ruta de la imagen del artículo"""
+    # Obtener la extensión del archivo
+    ext = filename.split('.')[-1]
+    # Generar nombre único basado en el título del artículo
+    filename = f"article_{instance.id}_{instance.title[:50]}.{ext}"
+    # Limpiar caracteres especiales
+    filename = "".join(c for c in filename if c.isalnum() or c in '._-')
+    return os.path.join('articles/', filename)
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
+    image = models.ImageField(
+        upload_to=article_image_path,
+        blank=True,
+        null=True,
+        help_text="Imagen principal del artículo (opcional)"
+    )
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         get_user_model(),
@@ -17,3 +34,8 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.id)])
+    
+    @property
+    def has_image(self):
+        """Verifica si el artículo tiene una imagen"""
+        return bool(self.image and hasattr(self.image, 'url'))
